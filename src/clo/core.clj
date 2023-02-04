76f2d453e881612a6ee7985dabb725701514128e
Franziska <franziska.loof@stud.th-luebeck.de>
2023-02-05 00:19:23 +0100

    base game

--- src/clo/core.clj
+++ src/clo/core.clj
@@ -1,7 +1,32 @@
 (ns clo.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn determine-winner [user-choice computer-choice]
+  (let [winning-combinations {
+                              "rock" "scissor"
+                              "paper" "rock"
+                              "scissor" "paper"}
+        user-winning-combination (winning-combinations user-choice)]
+    (if (= user-winning-combination computer-choice)
+        "Congratulations! You have won!"
+        (if (= user-choice computer-choice)
+          "It was a draw."
+          "You have lost. Better luck next time."))
+    )
+  )
+
+(defn start-game []
+  (let [choices ["rock" "paper" "scissor"]
+        computer-choice (nth choices (rand-int 3))
+        user-choice (read-line)
+        game-result (determine-winner user-choice computer-choice)]
+    (println "The computers choice: " computer-choice)
+    (println "Your choice: " user-choice)
+    (println game-result))
+  )
+
+(defn -main []
+    (println "Welcome to Rock Paper Scissor!")
+    (println "Please make a choice between 'rock', 'paper' and 'scissor'.")
+    (start-game)
+    )
